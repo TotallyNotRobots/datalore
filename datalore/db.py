f09d34d9fa6ea3f719f15c812e60ef22ca03e485
linuxdaemon <linuxdaemon.irc@gmail.com>
2020-10-12 01:44:15 -0400

    Update to 3.8

--- datalore/db.py
+++ datalore/db.py
@@ -1,6 +1,6 @@
 import os
 
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -9,11 +9,23 @@ __all__ = ("Session", "metadata", "engine", "Base")
 engine = create_engine(os.getenv("DB_URL", "sqlite:///datalore.db"))
 Session = scoped_session(sessionmaker(bind=engine))
 
-_Base = declarative_base(bind=engine)
+declarative_meta = declarative_base(bind=engine)
 
 
-class Base(_Base):
+class Base(declarative_meta):
     __abstract__ = True
 
+    def __repr__(self) -> str:
+        db_session = Session()
+        obj = db_session.merge(self)
+        obj_type = type(obj)
+        cols = [col.name for col in obj_type.__table__.columns]
+        return "{}({})".format(
+            obj_type.__name__,
+            ", ".join(
+                "{}={!r}".format(name, getattr(obj, name)) for name in cols
+            ),
+        )
 
-metadata: MetaData = Base.metadata
+
+metadata = Base.metadata
